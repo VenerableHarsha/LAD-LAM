b10ac77ddc8854104b9cf6cd1f5fad0570d41592
tahapasha767 <110926026+tahapasha767@users.noreply.github.com>
2025-04-01 23:37:11 +0530

    Add files via upload

---
+++ new_foler22/src/components/Page2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page2 = () => {
+  return (
+    <div className='h-screen w-screen md:w-[40%] bg-[#030013]'>
+      <div className='flex justify-end text-white font-bold text-lg p-4 '>BOARD</div>
+    </div>
+  )
+}
+
+export default Page2
